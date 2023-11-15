4b1483b85be4577ca6ff3914aabbc6c5e4daf75e
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-11-15 14:28:40 +0900

    カメラ実装

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -17,7 +17,7 @@ void RailCamera::Update() {
 
 	   if (target_) {
 
-		Vector3 offset = {0.0f, 8.0f, -20.0f};
+		Vector3 offset = {0.0f, 0.0f, -20.0f};
 
 		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(viewProjection_.rotation_.x);
 		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(viewProjection_.rotation_.y);
@@ -28,14 +28,29 @@ void RailCamera::Update() {
 		offset = TransformNormal(offset, rotateXYZMatrix);
 
 		viewProjection_.translation_ = Add(target_->translation_, offset);
-	}
+		//viewProjection_.translation_.y = 0.0f;
+	   
+	   }
+
+	   XINPUT_STATE joyState;
+
+	   if (Input::GetInstance()->GetJoystickState(0, joyState)) {
+
+		Vector3 rotationSpeed = {0.05f, 0.05f, 0.05f};
+
+		viewProjection_.rotation_.y +=
+		    (float)joyState.Gamepad.sThumbRX / SHRT_MAX * rotationSpeed.y;
+
+		viewProjection_.rotation_.z +=
+		    (float)joyState.Gamepad.sThumbRY / SHRT_MAX * rotationSpeed.x;
+	   }
 
 	worldTransform_.matWorld_ = MakeAffineMatrix(
 	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
 
 	viewProjection_.matView = Inverse(worldTransform_.matWorld_);
 
-	// viewProjection_.UpdateMatrix();
+	 viewProjection_.UpdateMatrix();
 	// viewProjection_.TransferMatrix();
 
 	ImGui::Begin("Camera");
